45b376e5e597018a1ca8b46b72a74e25c62f7869
SergioFLS <34007063+SergioFLS@users.noreply.github.com>
2023-02-17 20:52:16 -0300

    fix map-related segfault on local machine

--- src/main.c
+++ src/main.c
@@ -26,6 +26,7 @@ int main() {
     // Create game state structure
     Game *g = malloc(sizeof(Game));
     if (!g) error(g, "Failed to allocate game state");
+    memset(g, 0, sizeof(Game));
 
     // Initialize the game - this loads the window and assets.
     initGame(g);
